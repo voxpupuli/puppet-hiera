bb0d846ba7525b64754fa614b75de50e4ad35dbd
Hunter Haugen <h.haugen@gmail.com>
2012-08-14 11:44:55 -0700

    Release 0.1.0
    Features:
    - Template hiera.yaml hierarchy
    - Symlink /etc/hiera.yaml for `hiera` command line tool
    - Designed for PE, with parameters for POSS provisioning.
    
    Caveat:
    - Does not manage package

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'hunner-hiera'
+version '0.1.0'
+source 'UNKNOWN'
+author 'hunner'
+license 'UNKNOWN'
+summary 'UNKNOWN'
+description 'UNKNOWN'
+project_page 'https://github.com/hunner/Hiera-module-build'
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+class hiera(
+  $hierarchy  = [],
+  $hiera_yaml = '/etc/puppetlabs/puppet/hiera.yaml',
+  $datadir    = '/etc/puppetlabs/puppet/hieradata'
+) {
+  File {
+    owner => 'pe-puppet',
+    group => 'pe-puppet',
+    mode  => '0644',
+  }
+  file { $hiera_data:
+    ensure => directory,
+  }
+  # Template uses $hierarchy, $datadir
+  file { $hiera_yaml:
+    ensure  => present,
+    content => template('hiera/hiera.yaml.erb'),
+  }
+  # Symlink for hiera command line tool
+  file { "/etc/hiera.yaml":
+    ensure => symlink,
+    target => $hiera_yaml,
+  }
+}
