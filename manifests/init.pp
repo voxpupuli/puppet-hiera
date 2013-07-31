1735c1eecfcc6c9986d3d9530746274a9209ec77
David Goodlad <david@goodlad.net>
2013-07-31 21:00:43 +1000

    Revert "Conditionally manage the $datadir"
    This reverts commit fed14ca8e94685acba5142fabba6f92dc0f8a7b5.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,23 @@
 class hiera(
   $hierarchy  = [],
-  $hiera_yaml     = '/etc/puppet/hiera.yaml',
-  $datadir        = '/etc/puppet/hieradata',
-  $manage_datadir = false,
-  $owner          = 'puppet',
-  $group          = 'puppet'
+  $hiera_yaml = '/etc/puppetlabs/puppet/hiera.yaml',
+  $datadir    = '/etc/puppetlabs/puppet/hieradata',
+  $owner      = 'puppet',
+  $group      = 'puppet'
 ) {
   File {
     owner => $owner,
     group => $group,
     mode  => '0644',
   }
-
-  if $manage_datadir {
-    file { $datadir:
-      ensure => directory,
-    }
+  file { $datadir:
+    ensure => directory,
   }
-
   # Template uses $hierarchy, $datadir
   file { $hiera_yaml:
     ensure  => present,
     content => template('hiera/hiera.yaml.erb'),
   }
-
   # Symlink for hiera command line tool
   file { "/etc/hiera.yaml":
     ensure => symlink,
