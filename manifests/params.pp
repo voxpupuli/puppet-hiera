89cd0c82d12a9b3c6d47d1ddbd08ce1c7c31ddd3
Terri Haber <terri@puppetlabs.com>
2014-12-19 15:25:08 -0800

    syntax

--- manifests/params.pp
+++ manifests/params.pp
@@ -24,8 +24,8 @@ class hiera::params {
       $provider = 'pe_puppetserver_gem'
       # 3.7 also needs /opt/puppet/bin/eyaml
       exec { 'install pe_gem':
-          command => '/opt/puppet/bin/gem install hiera-eyaml'
-          creates => '/opt/puppet/bin/eyaml'
+          command => '/opt/puppet/bin/gem install hiera-eyaml',
+          creates => '/opt/puppet/bin/eyaml',
       }
     } else {
       $provider = 'pe_gem'
