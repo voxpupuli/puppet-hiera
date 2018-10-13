bc3b1ccbb8908c1ea823aaf05da804ded072ad69
Tim Meusel <tim@bastelfreak.de>
2018-10-13 16:05:14 +0200

    drop puppet 3 support

--- manifests/params.pp
+++ manifests/params.pp
@@ -36,27 +36,19 @@ class hiera::params {
       $master_service = 'pe-httpd'
     }
   } else {
-    if $::puppetversion and versioncmp($::puppetversion, '4.0.0') >= 0 {
-      # Configure for AIO packaging.
-      if getvar('::pe_server_version') {
-        $master_service = 'pe-puppetserver'
-        $provider       = 'puppetserver_gem'
-      } else {
-        # It would probably be better to assume this is puppetserver, but that
-        # would be a backwards-incompatible change.
-        $master_service = 'puppetmaster'
-        $provider       = 'puppet_gem'
-      }
-      $cmdpath        = ['/opt/puppetlabs/puppet/bin', '/usr/bin', '/usr/local/bin']
-      $datadir        = '/etc/puppetlabs/code/environments/%{::environment}/hieradata'
-      $manage_package = false
+    # Configure for AIO packaging.
+    if getvar('::pe_server_version') {
+      $master_service = 'pe-puppetserver'
+      $provider       = 'puppetserver_gem'
     } else {
+      # It would probably be better to assume this is puppetserver, but that
+      # would be a backwards-incompatible change.
       $master_service = 'puppetmaster'
-      $provider       = 'gem'
-      $cmdpath        = ['/usr/bin', '/usr/local/bin']
-      $datadir        = "${confdir}/hieradata"
-      $manage_package = true
+      $provider       = 'puppet_gem'
     }
+    $cmdpath        = ['/opt/puppetlabs/puppet/bin', '/usr/bin', '/usr/local/bin']
+    $datadir        = '/etc/puppetlabs/code/environments/%{::environment}/hieradata'
+    $manage_package = false
     if getvar('::pe_server_version') {
       $owner = 'pe-puppet'
       $group = 'pe-puppet'
