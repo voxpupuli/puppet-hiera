16a36fb497bd86bb7958cef05a70fc9874b37e05
Lars Bahner <lars.bahner@gmail.com>
2020-10-12 04:08:50 +0200

    Changing layout to meet strict requirements. This is kindoff a guessing game.

--- spec/classes/eyaml_gpg_spec.rb
+++ spec/classes/eyaml_gpg_spec.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
 require 'spec_helper'
 require 'puppet'
@@ -6,13 +6,17 @@ require 'puppet'
 describe 'hiera::eyaml_gpg' do
 
   context 'default params' do
+
     let(:pre_condition) do
       'class { "hiera": eyaml_gpg  => true, keysdir => "/dev/null/keys" }'
+
     end
+
     it { is_expected.to compile }
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_file('/dev/null/keys') }
-    it { is_expected.to contain_file('/dev/null/keys/gpg').with(
+    it { is_expected.to contain_file('/dev/null/keys/gpg').
+      with(
         'ensure'  => 'directory',
         'recurse' => true,
         'purge'   => false,
@@ -21,15 +25,21 @@ describe 'hiera::eyaml_gpg' do
     }
     it { is_expected.to contain_package('hiera-eyaml-gpg')}
     it { is_expected.to contain_package('ruby_gpg')}
+
   end
 
   context 'eyaml_gpg_gnupghome_recurse is false' do
+
     let(:pre_condition) do
+
       'class { "hiera": eyaml_gpg  => true, keysdir => "/dev/null/keys", eyaml_gpg_gnupghome_recurse => false,}'
     end
-    it { is_expected.to contain_file('/dev/null/keys/gpg').with(
+
+    it { is_expected.to contain_file('/dev/null/keys/gpg').
+      with(
         'recurse' => false,
       )
     }
+
   end
 end
