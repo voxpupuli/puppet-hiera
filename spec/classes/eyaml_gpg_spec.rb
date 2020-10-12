23b12d84e825f93791e4215bf0f3e1f683476c99
Lars Bahner <lars.bahner@gmail.com>
2020-10-12 03:04:12 +0200

    Add test for eyaml_gpg and some other minor checks
    I found some checks missing from rake runs, and fixed
    them. Doing this I mangled hiera_spec.rb to achieve
    consistent naming of keysdir. When I started I used
    default and that caused 3 different namings of the
    same directory. That seems silly.
    
    The reason it ended up being /dev/null/keys is that
    theam YAML eyaml loader sets confdir to "/dev/null"
    The string "/dev/null" is not mentioned anywhere in
    the code. So I went with it and changed keysdir to
    /dev/null/keys instead of /etc/keys. This is a small
    change, that increases the coverage percentage when
    I check some file in the eyaml_gpg_spec.rb.
    
    Also added a check for the deep_merge class, just
    becasue it was easy and on my way. I am getting the
    hang of this now - but if you would ask me what I
    have done and how, I would have to do answer in a
    Mr. Miagi-style.
    
      Don't know. Firsta time.

---
+++ spec/classes/eyaml_gpg_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: false
+
+require 'spec_helper'
+require 'puppet'
+
+describe 'hiera::eyaml_gpg' do
+
+
+  context 'default params' do
+    let(:pre_condition) do
+      'class { "hiera": eyaml_gpg  => true, keysdir => "/dev/null/keys" }'
+    end
+    it { is_expected.to compile }
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_file('/dev/null/keys') }
+    it { is_expected.to contain_file('/dev/null/keys/gpg')
+      .with(
+        'ensure'  => 'directory',
+        'recurse' => true,
+        'purge'   => false,
+        'mode'    => '0600',
+      )}
+    it { is_expected.to contain_package('hiera-eyaml-gpg')}
+    it { is_expected.to contain_package('ruby_gpg')}
+  end
+
+  context 'eyaml_gpg_gnupghome_recurse is false' do
+    let(:pre_condition) do
+      'class { "hiera": eyaml_gpg  => true, keysdir => "/dev/null/keys", eyaml_gpg_gnupghome_recurse => false,}'
+    end
+    it { is_expected.to contain_file('/dev/null/keys/gpg')
+      .with(
+        'recurse' => false,
+      )}
+
+  end
+end
