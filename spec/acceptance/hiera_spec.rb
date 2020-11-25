afbcd51c159d291a9ef0753288df614ddd7b4396
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-11-26 00:14:45 +0100

    Update acceptance tests to run

--- spec/acceptance/hiera_spec.rb
+++ spec/acceptance/hiera_spec.rb
@@ -1,26 +1,10 @@
 require 'spec_helper_acceptance'
 
 describe 'hiera' do
-  version = on(master, puppet('--version')).stdout
-  case version
-  when %r{Puppet Enterprise 3}
-    confdir = '/etc/puppetlabs/puppet'
-    datadir = "#{confdir}/hieradata"
-    actualdir = datadir
-    manifestsdir = "#{confdir}/environments/production/manifests"
-  when %r{^3}
-    confdir = '/etc/puppet'
-    datadir = "#{confdir}/hieradata"
-    actualdir = datadir
-    manifestsdir = "#{confdir}/manifests"
-  when %r{^4}
-    confdir = '/etc/puppetlabs/puppet'
-    datadir = '/etc/puppetlabs/code/environments/%{::environment}/hieradata'
-    actualdir = '/etc/puppetlabs/code/environments/production/hieradata'
-    manifestsdir = '/etc/puppetlabs/code/environments/production/manifests'
-  else
-    raise "Unknown puppet version #{version}"
-  end
+  confdir = '/etc/puppetlabs/puppet'
+  datadir = '/etc/puppetlabs/code/environments/%{::environment}/hieradata'
+  actualdir = '/etc/puppetlabs/code/environments/production/hieradata'
+  manifestsdir = '/etc/puppetlabs/code/environments/production/manifests'
   hierayaml = "#{confdir}/hiera.yaml"
 
   describe 'puppet apply' do
@@ -38,18 +22,18 @@ describe 'hiera' do
         ],
       }
       EOS
-      apply_manifest_on(master, pp, catch_failures: true)
-      apply_manifest_on(master, pp, catch_changes: true)
+      apply_manifest_on(default, pp, catch_failures: true)
+      apply_manifest_on(default, pp, catch_changes: true)
     end
   end
-  describe file(hierayaml), node: master do
+  describe file(hierayaml), node: default do
     its(:content) do
       is_expected.to match <<-EOS
 # managed by puppet
 ---
 :backends:
-  - eyaml
-  - yaml
+- eyaml
+- yaml
 
 :logger: console
 
@@ -58,20 +42,22 @@ describe 'hiera' do
   - "?nodes/%{::trusted.certname}"?
   - common
 
-:yaml:
-  :datadir: #{datadir}
-
 :eyaml:
-  :datadir: #{datadir}
-  :pkcs7_private_key: #{confdir}/keys/private_key.pkcs7.pem
-  :pkcs7_public_key:  #{confdir}/keys/public_key.pkcs7.pem
+  :datadir: "?#{datadir}"?
+  :pkcs7_private_key: "?#{confdir}/keys/private_key.pkcs7.pem"?
+  :pkcs7_public_key: "?#{confdir}/keys/public_key.pkcs7.pem"?
+
+:yaml:
+  :datadir: "?#{datadir}"?
+
+:merge_behavior: deep
 EOS
     end
   end
   describe 'querying hiera' do
     before :all do
-      on master, "mkdir -p #{actualdir}/production"
-      on master, "echo myclass::value: 'found output' > #{actualdir}/common.yaml"
+      on default, "mkdir -p #{actualdir}/production"
+      on default, "echo myclass::value: 'found output' > #{actualdir}/common.yaml"
     end
     let(:pp) do
       <<-EOS
@@ -84,14 +70,14 @@ EOS
     end
 
     it 'finds it on the command line' do
-      expect(on(master, 'hiera myclass::value environment=production').stdout.strip).to eq('found output')
+      expect(on(default, 'hiera myclass::value environment=production').stdout.strip).to eq('found output')
     end
     it 'finds it in puppet apply' do
-      expect(apply_manifest_on(master, pp, catch_failures: true).stdout.strip).to match(%r{found output})
+      expect(apply_manifest_on(default, pp, catch_failures: true).stdout.strip).to match(%r{found output})
     end
-    it 'finds it in puppet agent', if: (master.is_pe? || master[:roles].include?('aio')) do
-      make_site_pp(pp, manifestsdir)
-      expect(on(master, puppet('agent', '-t', '--server', '$(hostname -f)'), acceptable_exit_codes: [0, 2]).stdout.strip).to match(%r{found output})
+    it 'finds it in puppet agent' do
+      make_site_pp(default, pp, manifestsdir)
+      expect(on(default, puppet('agent', '-t', '--server', '$(hostname -f)'), acceptable_exit_codes: [0, 2]).stdout.strip).to match(%r{found output})
     end
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,30 +2,30 @@ require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
   install_module_from_forge_on(host, 'puppetlabs-puppetserver_gem', '>= 0')
-  if ENV['PUPPET_INSTALL_TYPE'] == 'agent' && ENV['BEAKER_provision'] != 'no'
+  unless ENV['BEAKER_provision'] == 'no'
     install_package(host, 'puppetserver')
     on host, puppet('resource', 'service', 'puppetserver', 'ensure=running')
   end
 end
 
-def wait_for_master(max_retries)
+def wait_for_puppetserver(host, max_retries)
   1.upto(max_retries) do |retries|
-    on(master, "curl -skIL https://#{master.hostname}:8140", acceptable_exit_codes: [0, 1, 7]) do |result|
+    on(host, "curl -skIL https://#{host.hostname}:8140", acceptable_exit_codes: [0, 1, 7]) do |result|
       return true if result.stdout =~ %r{HTTP/1\.1 4}
 
       counter = 2**retries
-      logger.debug "Unable to reach Puppet Master, #{master.hostname}, Sleeping #{counter} seconds for retry #{retries}..."
+      logger.debug "Unable to reach Puppet Server, #{host.hostname}, Sleeping #{counter} seconds for retry #{retries}..."
       sleep counter
     end
   end
-  raise 'Could not connect to Puppet Master.'
+  raise 'Could not connect to Puppet Server.'
 end
 
-def make_site_pp(pp, path = File.join(master['puppetpath'], 'manifests'))
-  on master, "mkdir -p #{path}"
-  create_remote_file(master, File.join(path, 'site.pp'), pp)
-  on master, "chown -R #{puppet_user(master)}:#{puppet_group(master)} #{path}"
-  on master, "chmod -R 0755 #{path}"
-  on master, "service #{(master['puppetservice'] || 'puppetserver')} restart"
-  wait_for_master(3)
+def make_site_pp(host, pp, path)
+  on host, "mkdir -p #{path}"
+  create_remote_file(host, File.join(path, 'site.pp'), pp)
+  on host, "chown -R #{puppet_user(host)}:#{puppet_group(host)} #{path}"
+  on host, "chmod -R 0755 #{path}"
+  on host, 'service puppetserver restart'
+  wait_for_puppetserver(host, 3)
 end
